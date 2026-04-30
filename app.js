e278974a9702f2fbefdf68b261e43d94ec758329
abdulloh <abdullohmamasiddiov009@gamil.com>
2026-04-30 15:42:08 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const themeToggle = document.getElementById("toggle"); 
+const themeText = document.getElementById("dark");
+const dropdownHeader = document.getElementById("cbd"); 
+const dropdownList = document.getElementById("davlatlar"); 
+const selectedRegion = document.getElementById("region"); 
+const listItems = document.querySelectorAll("#davlatlar li");
+const body = document.body;
+
+
+const savedTheme = localStorage.getItem("theme");
+if (savedTheme === "dark") {
+  body.classList.add("dark-mode");
+  themeText.innerText = "Light Mode";
+}
+
+
+themeToggle.addEventListener("click", () => {
+  body.classList.toggle("dark-mode"); 
+
+  if (body.classList.contains("dark-mode")) {
+    themeText.innerText = "Light Mode";
+    localStorage.setItem("theme", "dark");
+  } else {
+    themeText.innerText = "Dark Mode";
+    localStorage.setItem("theme", "light");
+  }
+});
+
+
+dropdownHeader.addEventListener("click", (e) => {
+  e.stopPropagation();
+  dropdownList.classList.toggle("active"); 
+});
+
+
+listItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    selectedRegion.innerText = item.innerText;
+    dropdownList.classList.remove("active");
+  });
+});
+
+
+window.addEventListener("click", () => {
+  dropdownList.classList.remove("active");
+});
