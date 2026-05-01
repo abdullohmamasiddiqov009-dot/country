aae7740b21273d63f77846c5eb468a3c1bacb80c
abdulloh <abdullohmamasiddiov009@gamil.com>
2026-05-01 09:14:19 +0300

    yuklandi

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const themeToggle = document.getElementById("toggle"); 
+const themeToggle = document.getElementById("toggle");
 const themeText = document.getElementById("dark");
-const dropdownHeader = document.getElementById("cbd"); 
-const dropdownList = document.getElementById("davlatlar"); 
-const selectedRegion = document.getElementById("region"); 
+const dropdownHeader = document.getElementById("cbd");
+const dropdownList = document.getElementById("davlatlar");
+const selectedRegion = document.getElementById("region");
 const listItems = document.querySelectorAll("#davlatlar li");
+const countresWrap = document.querySelector(".country-list");
 const body = document.body;
-
 
 const savedTheme = localStorage.getItem("theme");
 if (savedTheme === "dark") {
@@ -13,9 +13,8 @@ if (savedTheme === "dark") {
   themeText.innerText = "Light Mode";
 }
 
-
 themeToggle.addEventListener("click", () => {
-  body.classList.toggle("dark-mode"); 
+  body.classList.toggle("dark-mode");
 
   if (body.classList.contains("dark-mode")) {
     themeText.innerText = "Light Mode";
@@ -26,12 +25,10 @@ themeToggle.addEventListener("click", () => {
   }
 });
 
-
 dropdownHeader.addEventListener("click", (e) => {
   e.stopPropagation();
-  dropdownList.classList.toggle("active"); 
+  dropdownList.classList.toggle("active");
 });
-
 
 listItems.forEach((item) => {
   item.addEventListener("click", () => {
@@ -40,7 +37,19 @@ listItems.forEach((item) => {
   });
 });
 
-
 window.addEventListener("click", () => {
   dropdownList.classList.remove("active");
+});
+import countries from "./main.js";
+console.log(countries);
+countries.forEach((country) => {
+  countresWrap.innerHTML += `  <div class="country">
+        <img src="${country.image}" alt="" />
+        <div class="country-intro">
+          <h3>${country.name}</h3>
+          <p>Population <span>${country.population}</span></p>
+          <p>Region <span>${country.region}</span></p>
+          <p>Capital <span>${country.capital} </span></p>
+        </div>
+      </div>`;
 });
